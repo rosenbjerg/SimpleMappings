589913958d2e2df233bf1264af32772a59318813
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2020-02-18 01:16:08 +0100

    More unit tests

--- SimpleMappings.Tests/Tests.cs
+++ SimpleMappings.Tests/Tests.cs
@@ -99,11 +99,12 @@ namespace SimpleMappings.Tests
         {
             public Mapping<MyClass, MyClassDto> MyClassMapper => MappingBuilder<MyClass, MyClassDto>.New()
                 .MapProperty(mc => mc.Structs.Sum(s => s.Stat), mcd => mcd.sumOfStructs)
+                .AutomapRemaining()
                 .UsingFactory<MyClassDto>();
         }
 
         [Test]
-        public void BasicMapperTest()
+        public void ReflectedMapperTest()
         {
             var mapper = new Mapper();
 
@@ -116,7 +117,32 @@ namespace SimpleMappings.Tests
             };
 
             var dto = mapper.Map<MyClass, MyClassDto>(model);
+            
+            Assert.AreEqual(model.Age, dto._age);
+            Assert.AreEqual(model.Name, dto.name);
+            Assert.AreEqual(model.Numbers.Count, dto.numbers.Count());
+            Assert.AreEqual(model.Structs.Length, dto.structs.Count);
+            Assert.AreEqual(model.Structs.Sum(s => s.Stat), dto.sumOfStructs);
+        }
+        [Test]
+        public void PropertyMapperTest()
+        {
+            var mapper = new Mapper();
 
+            var model = new MyClass
+            {
+                Age = 23,
+                Name = "daw",
+                Numbers = new List<int> {1, 2, 3},
+                Structs = new[] {new MyStruct {Stat = 2.3}, new MyStruct {Stat = 5.4}}
+            };
+
+            var dto = mapper.MyClassMapper.Map(model);
+
+            Assert.AreEqual(model.Age, dto._age);
+            Assert.AreEqual(model.Name, dto.name);
+            Assert.AreEqual(model.Numbers.Count, dto.numbers.Count());
+            Assert.AreEqual(model.Structs.Length, dto.structs.Count);
             Assert.AreEqual(model.Structs.Sum(s => s.Stat), dto.sumOfStructs);
         }
     }
